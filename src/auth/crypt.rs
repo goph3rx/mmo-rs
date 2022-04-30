04ef3bd8182a99f6e8ee398dd421d90f38086831
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-04-30 09:15:56 +0000

    Scramble Init message (#5)

--- src/auth/crypt.rs
+++ src/auth/crypt.rs
@@ -1,4 +1,7 @@
-/// Scramble the modulus for username/password encryption.
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::{Cursor, Result};
+use std::num::Wrapping;
+
 pub fn scramble_modulus(modulus: &mut [u8]) {
     for i in 0..4 {
         modulus.swap(i, i + 77);
@@ -14,6 +17,23 @@ pub fn scramble_modulus(modulus: &mut [u8]) {
     }
 }
 
+const BLOCK_SIZE: usize = 4;
+
+pub fn scramble_init(buffer: &mut [u8], size: usize, key: i32) -> Result<()> {
+    // Scramble
+    let mut key = Wrapping(key);
+    for offset in (BLOCK_SIZE..size).step_by(BLOCK_SIZE) {
+        let mut block = Cursor::new(&buffer[offset..]).read_i32::<LittleEndian>()?;
+        key += block;
+        block ^= key.0;
+        Cursor::new(&mut buffer[offset..]).write_i32::<LittleEndian>(block)?;
+    }
+
+    // Write the key
+    Cursor::new(&mut buffer[size..]).write_i32::<LittleEndian>(key.0)?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,7 +42,7 @@ mod tests {
     fn scramble_modulus_success() {
         // Arrange
         let mut modulus = hex::decode("9a277669023723947d0ebdccef967a24c715018df6ce66414fccd0f5bab54124b8caac6d7f52f8bbbab7de926b4f0ac4cc84793196e44928774a57737d0e4ee02962952257506e898846e353fa5fee31409a1d32124fb8df53d969dd7aa222866fa85e106f8a07e333d8ded4b10a8300b32d5f47cc5eab14033fa2bc0950b5c9").
-            expect("Fail to decode modulus");
+            expect("Failed to decode modulus");
 
         // Act
         scramble_modulus(&mut modulus);
@@ -32,5 +52,44 @@ mod tests {
             hex::encode(modulus),
             "768ca46255674d1df5485e9f1556e7b0928f1cbfe481de9e1c15b928c01763a2d762f27d10d8ff58896f0046da4589c47fa926765abae23c7475f5cf745efb295fee3140023723947d0ebdccefccc0c6fb15018df6ce66414fccd0f5bab54124b8caac6d7f52f8bbbab7de926b4f0ac4cc84793196e44928774a57737d0e4ee0",
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn scramble_modulus_fail() {
+        // Arrange
+        let mut modulus = vec![0];
+
+        // Act
+        scramble_modulus(&mut modulus);
+    }
+
+    #[test]
+    fn scramble_init_success() {
+        // Arrange
+        let mut buffer = hex::decode("010203040506070800000000").expect("Failed to decode buffer");
+        let size = buffer.len() - BLOCK_SIZE;
+        let key = -559038737;
+
+        // Act
+        let result = scramble_init(&mut buffer, size, key);
+
+        // Assert
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(hex::encode(buffer), "01020304f1c2b3eef4c4b4e6");
+    }
+
+    #[test]
+    fn scramble_init_fail() {
+        // Arrange
+        let mut buffer = hex::decode("0102030405060708").expect("Failed to decode buffer");
+        let size = buffer.len();
+        let key = -559038737;
+
+        // Act
+        let result = scramble_init(&mut buffer, size, key);
+
+        // Assert
+        assert_eq!(result.is_err(), true);
     }
 }
