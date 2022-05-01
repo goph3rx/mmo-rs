5f7861506cd1a8031ef748857cb017ac35d0cd3a
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-05-01 14:42:40 +0000

    Server message for GG auth

--- src/auth/message.rs
+++ src/auth/message.rs
@@ -3,11 +3,20 @@ use std::io::{Result, Seek, SeekFrom, Write};
 
 use crate::io::WriteMMO;
 
+#[derive(Debug)]
+pub enum GGAuthResult {
+    Skip = 0x0b,
+}
+
+#[derive(Debug)]
 pub enum ServerMessage {
     Init {
         session_id: i32,
         modulus: [u8; 128],
         crypt_key: [u8; 16],
+    },
+    GGAuth {
+        result: GGAuthResult,
     },
 }
 
@@ -29,17 +38,21 @@ pub fn encode(msg: ServerMessage, io: &mut (impl Write + Seek)) -> Result<()> {
             io.seek(SeekFrom::Current(16))?;
             io.write_b(&crypt_key)?;
         }
+        ServerMessage::GGAuth { result } => {
+            io.write_c(0x0b)?;
+            io.write_d(result as i32)?;
+            io.seek(SeekFrom::Current(16))?;
+        }
     }
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::auth::BUFFER_SIZE;
     use std::io::Cursor;
 
     use super::*;
-
-    const BUFFER_SIZE: usize = 1024;
 
     #[test]
     fn server_init() {
@@ -66,5 +79,27 @@ mod tests {
         assert_eq!(result.is_ok(), true);
         assert_eq!(position, 1 + 4 + 4 + 128 + 16 + 16);
         assert_eq!(hex::encode(&buffer[..position]), "00efbeadde21c60000768ca46255674d1df5485e9f1556e7b0928f1cbfe481de9e1c15b928c01763a2d762f27d10d8ff58896f0046da4589c47fa926765abae23c7475f5cf745efb295fee3140023723947d0ebdccefccc0c6fb15018df6ce66414fccd0f5bab54124b8caac6d7f52f8bbbab7de926b4f0ac4cc84793196e44928774a57737d0e4ee0000000000000000000000000000000000102030405060708090a0b0c0d0e0f10");
+    }
+
+    #[test]
+    fn server_gg_auth() {
+        // Arrange
+        let mut buffer = vec![0; BUFFER_SIZE];
+        let mut writer = Cursor::new(&mut buffer);
+        let msg = ServerMessage::GGAuth {
+            result: GGAuthResult::Skip,
+        };
+
+        // Act
+        let result = encode(msg, &mut writer);
+
+        // Assert
+        let position = writer.position() as usize;
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(position, 1 + 4 + 16);
+        assert_eq!(
+            hex::encode(&buffer[..position]),
+            "0b0b00000000000000000000000000000000000000"
+        );
     }
 }
--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,3 +1,14 @@
 //! Auth server implementation.
 mod crypt;
 mod message;
+
+/// Size of the packet header.
+pub const HEADER_SIZE: usize = 2;
+/// Size of the buffers for IO, packet bodies cannot exceed this.
+pub const BUFFER_SIZE: usize = 1024;
+/// Size of the block for IO operations.
+pub const BLOCK_SIZE: usize = 4;
+/// Initial encryption key for the traffic.
+pub const INIT_KEY: &[u8] = &[
+    0x6B, 0x60, 0xCB, 0x5B, 0x82, 0xCE, 0x90, 0xB1, 0xCC, 0x2B, 0x6C, 0x55, 0x6C, 0x6C, 0x6C, 0x6C,
+];
